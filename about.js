3b48ca1b8623bbf7b38652b4d8e93fb776632a4c
unknown <novikov.nicita123@gmail.com>
2024-04-12 19:32:30 +0300

    Verstka final

---
+++ about.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function () {
+	let aboutSection = document.getElementById('aboutSection')
+	aboutSection.querySelector('.about').classList.add('visible')
+})
+console.log('1234')
